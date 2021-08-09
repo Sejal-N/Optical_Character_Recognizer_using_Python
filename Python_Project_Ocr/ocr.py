a6955692589005e1605cf32b26d6b0724871bd42
Sejal Nagbhidkar <74907587+Sejal-N@users.noreply.github.com>
2021-08-09 13:44:38 +0530

    Add files via upload

---
+++ Python_Project_Ocr/ocr.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+import pytesseract
+from docx import Document
+
+
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+img = cv2.imread("bigsleep.jpg")
+text = pytesseract.image_to_string(img)
+print(text, file=open("Demo.txt", "a"))
+f = open('Demo.doc', "w")
+f.write(text)
+f.close()
+# 2. Resize the image
+img = cv2.resize(img, None, fx=0.5, fy=0.5)
+
+# 3. Convert image to grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# 4. Convert image to black and white (using adaptive threshold)
+adaptive_threshold = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 85, 11)
+
+config = "--psm 3"
+text = pytesseract.image_to_string(adaptive_threshold, config=config, lang="eng")
+print(text)
+
+cv2.imshow("gray", gray)
+cv2.imshow("adaptive th", adaptive_threshold)
+cv2.waitKey(0)
